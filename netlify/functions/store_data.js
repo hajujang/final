d8eae09d01bdcb30bd32e073f737c06fd453e2a9
swookkim <sangwook.kim@kellogg.northwestern.edu>
2021-05-29 17:30:30 -0500

    1

---
+++ netlify/functions/store_data.js
@@ -0,0 +1,26 @@
+
+// allows us to use firebase
+let firebase = require(`./firebase`)
+
+// /.netlify/functions/create_comment?postId=xxxxxxxxx&userName=Brian&body=Tacos!
+exports.handler = async function(event) {
+
+  let userName = event.queryStringParameters.userName
+  let globalPrice = event.queryStringParameters.globalPrice
+
+
+  // establish a connection to firebase in memory
+  let db = firebase.firestore()
+
+  // create a new post, wait for it to return
+  await db.collection('items').add({
+    userName: userName,
+    globalPrice: globalPrice,
+    created: firebase.firestore.FieldValue.serverTimestamp()
+  })
+
+  return {
+    statusCode: 200
+  }
+}
+
