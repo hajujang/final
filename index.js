4d35f392877e52d75d74aa93c7b95abf90da1b8f
swookkim <sangwook.kim@kellogg.northwestern.edu>
2021-06-01 12:01:21 -0500

    1

--- index.js
+++ index.js
@@ -54,7 +54,7 @@
   displayPriceElement.insertAdjacentHTML(`beforeend`,`
   <div class= "border-2 p-4">
   <div class="font-bold"> Current price of 1 ${coin} in the Korean exchange: USD${koreaToGlobal}(KRW${koreaPrice}).
-  <div class="font-bold"> Current price of 1 ${coin} in the US exchange: USD${globalPrice}(KRW${koreaPrice}).
+  <div class="font-bold"> Current price of 1 ${coin} in the US exchange: USD${globalPrice}(KRW${globalToKorea}).
   <div class="font-bold"> Free retun per 1 ${coin} transaction with the US investors: USD${koreaToGlobal-globalPrice}(KRW${koreaPrice-globalToKorea}).
   </div>`
   )
