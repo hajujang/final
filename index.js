988161dbdebea2ec20bcf17bfb8cb8403d0ca552
swookkim <sangwook.kim@kellogg.northwestern.edu>
2021-06-01 10:49:50 -0500

    1

--- index.js
+++ index.js
@@ -47,30 +47,25 @@
   // console.log(KoreaToGlobalYield)
   // console.log(globalToKorea)
 
-  // Store a reference to the "korea-price element"
-  let koreaElement = document.querySelector(`.korea-price`)
+  // Store a reference to the "price elements"
+  let koreaElement = document.querySelector(`.display-price`)
 
-  
+  let globalElement = document.querySelector(`.display-price`)
+
+  let freeElement = document.querySelector(`.display-price`)
 
   // Fill the global element with the price
-  koreaElement.insertAdjacentHTML(`beforeend`,`
+  koreaElement.insertAdjacentHTML(`afterend`,`
   <div class="font-bold"> Current price of 1 ${coin} in the Korean exchange: USD${koreaToGlobal}(KRW${koreaPrice}).</div>`)
 
-  // Store a reference to the "global-price element"
-  let globalElement = document.querySelector(`.global-price`)
-
   // Fill the global element with the price
-  globalElement.insertAdjacentHTML(`beforeend`,`
+  globalElement.insertAdjacentHTML(`afterend`,`
   <div class="font-bold"> Current price of 1 ${coin} in the US exchange: USD${globalPrice}(KRW${koreaPrice}).</div>`)
 
-  // Store a reference to the "free-return element"
-  let freeElement = document.querySelector(`.premium`)
-
   // Fill the free element with the price
-  freeElement.insertAdjacentHTML(`beforeend`,`
+  freeElement.insertAdjacentHTML(`aftereend`,`
   <div class="font-bold"> Free retun per 1 ${coin} transaction with the US investors: USD${koreaToGlobal-globalPrice}(KRW${koreaPrice-globalToKorea}).</div>`)
   
-  if ()
   })
 
 firebase.auth().onAuthStateChanged(async function(user) {
